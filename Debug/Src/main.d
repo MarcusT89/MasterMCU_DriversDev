3c683b715338755cd9070461db5fb28fc89f045b
MarcusT89 <marcus.torres89@gmail.com>
2022-07-08 16:18:13 +0100

    First Project Commit

---
+++ Debug/Src/main.d
@@ -0,0 +1,4 @@
+Src/main.o: ../Src/main.c \
+ C:/Users/MarcusT/STM32CubeIDE/workspace_1.8.0/stm32f4xx_drivers/drivers/Inc/stm32f446xx.h
+
+C:/Users/MarcusT/STM32CubeIDE/workspace_1.8.0/stm32f4xx_drivers/drivers/Inc/stm32f446xx.h:
---
+++ drivers/Inc/stm32f446xx.h
@@ -0,0 +1,13 @@
+/*
+ * stm32f446xx.h
+ *
+ *  Created on: Jul 8, 2022
+ *      Author: MarcusT
+ */
+
+#ifndef INC_STM32F446XX_H_
+#define INC_STM32F446XX_H_
+
+
+
+#endif /* INC_STM32F446XX_H_ */
