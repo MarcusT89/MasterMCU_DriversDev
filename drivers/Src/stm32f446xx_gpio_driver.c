290f3e75023ccf8e529e13d7b4127d196f34f023
MarcusT89 <marcus.torres89@gmail.com>
2022-07-18 18:36:03 +0100

    update file names error

--- drivers/Src/stm32f446xx_gpio_driver.c
+++ drivers/Src/stm32f446xx_gpio_driver.c
@@ -1,9 +1,9 @@
 /*
- * stm32f446xx_gpio_driver.h
+ * stm32f446xx_gpio_driver.c
  *
  *  Created on: 18/07/2022
  *      Author: MarcusT
  */
 
 
-#include "stm32f446xx_gpio_driver.h"
+#include "stm32f446xx_gpio_driver.c"
