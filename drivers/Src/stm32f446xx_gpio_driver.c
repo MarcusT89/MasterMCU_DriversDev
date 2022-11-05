759cc89b0f75abc4609934b4c19781606308b4ef
MarcusT89 <marcus.torres89@gmail.com>
2022-11-05 03:54:25 +0000

    Update stm32f446xx_gpio_driver.c
    Adding configuration to the GPIO port selection in SYSCFG_EXTICR

--- drivers/Src/stm32f446xx_gpio_driver.c
+++ drivers/Src/stm32f446xx_gpio_driver.c
@@ -111,6 +111,11 @@ void GPIO_Init(GPIO_Handle_t *pGPIOHandle)
 		  EXIT->RTSR |= ( 1 << pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber);
 	  }
 	  // 2. Configure the GPIO port selection in SYSCFG_EXTICR
+	  uint8_t temp1 = pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber / 4; // (AULA:111)
+	  uint8_t temp2 = pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber % 4;
+	  uint8_t portcode = GPIO_BASEADDR_TO_CODE(pGPIOHandle->pGPIOx);
+	  SYSCFG_PCLK_EN();
+	  SYSCFG->EXTICR[TEMP1] = portcode << (temp2 * 4);
 
 	  // 3. enable the EXTI interrupt delivery using IMR
 	  EXIT->IMR |= ( 1 << pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber);
