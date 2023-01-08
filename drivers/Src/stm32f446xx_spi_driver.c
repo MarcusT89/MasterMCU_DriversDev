94575f28ea225000cbbf72c71f273b41dd5136a8
MarcusT89 <marcus.torres89@gmail.com>
2023-01-08 07:17:23 +0000

    Starting SPI interrupt

--- drivers/Src/stm32f446xx_spi_driver.c
+++ drivers/Src/stm32f446xx_spi_driver.c
@@ -207,16 +207,62 @@ void SPI_ReceiveData(SPI_RegDef_t *pSPIx, uint8_t *pRxBuffer, uint32_t Len)
 */
 void SPI_IRQInterruptConfig(uint8_t IRQNumber, uint8_t ENorDI)
 {
+	if (ENorDI == ENABLE)
+	{
+		if(IRQNumber <= 31)
+		{
+			//program ISER0 register
+			*NVIC_ISER0 |= ( 1 << IRQNumber );
+		}
+		else if (IRQNumber > 31 && IRQNumber < 64)
+		{
+			//program ISER1 register // 32 to 63
+			*NVIC_ISER1 |= ( 1 << IRQNumber % 32 );
+		}
+		else if (IRQNumber >= 64 && IRQNumber < 96)
+		{
+			//program ISER2 register //64 to 95
+			*NVIC_ISER2 |= ( 1 << IRQNumber % 64 );
+		}
 
+	} else
+	{
+		if(IRQNumber <= 31)
+				{
+					//program ICER0 register
+					*NVIC_ICER0 |= ( 1 << IRQNumber );
+				}
+				else if (IRQNumber > 31 && IRQNumber < 64)
+				{
+					//program ICER1 register // 32 to 63
+					*NVIC_ICER1 |= ( 1 << IRQNumber % 32 );
+				}
+				else if (IRQNumber >= 64 && IRQNumber < 96)
+				{
+					//program ICER2 register //64 to 95
+					*NVIC_ICER2 |= ( 1 << IRQNumber % 64 );
+				}
+	}
 }
+
 void SPI_IRQPriorityConfig(uint8_t IRQNumber, uint32_t IRQPriority)
 {
+	//1. lets find out the ipr regsiter
+	uint8_t iprx = IRQNumber / 4;
+	uint8_t iprx_section = IRQNumber % 4;
 
+	uint8_t shift_amount = ( 8 * iprx_section ) + (8 - NO_PR_BITS_IMPLEMENTED);
+
+	*(NVIC_PR_BASE_ADDR + iprx) |= (IRQPriority << shift_amount);
 }
+
 void SPI_IRQHandling(SPI_Handle_t *pHandle)
 {
 
 }
+
+
+
 
 
 void SPI_PeripheralControl(SPI_RegDef_t *pSPIx, uint8_t ENorDI)
