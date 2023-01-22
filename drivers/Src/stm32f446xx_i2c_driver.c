b38374f1a1a59b96c04596949bed548f4a04b619
MarcusT89 <marcus.torres89@gmail.com>
2023-01-22 17:29:48 +0000

    developing I2C_driver.c

--- drivers/Src/stm32f446xx_i2c_driver.c
+++ drivers/Src/stm32f446xx_i2c_driver.c
@@ -6,3 +6,97 @@
  */
 
 
+void I2C_Init(I2C_Handle_t *pI2CHandle)
+{
+	uint32_t tempreg = 0 ;
+
+	//enable the clock for the i2cx peripheral
+	I2C_PeriClockControl(pI2CHandle->pI2Cx,ENABLE);
+
+	//ack control bit
+	tempreg |= pI2CHandle->I2C_Config.I2C_AckControl << 10;
+	pI2CHandle->pI2Cx->CR1 = tempreg;
+
+	//configure the FREQ field of CR2
+	tempreg = 0;
+	tempreg |= RCC_GetPCLK1Value() /1000000U ;
+	pI2CHandle->pI2Cx->CR2 =  (tempreg & 0x3F);
+
+   //program the device own address
+	tempreg = 0;
+	tempreg |= pI2CHandle->I2C_Config.I2C_DeviceAddress << 1;
+	tempreg |= ( 1 << 14);
+	pI2CHandle->pI2Cx->OAR1 = tempreg;
+
+	//CCR calculations
+	uint16_t ccr_value = 0;
+	tempreg = 0;
+	if(pI2CHandle->I2C_Config.I2C_SCLSpeed <= I2C_SCL_SPEED_SM)
+	{
+		//mode is standard mode
+		ccr_value = (RCC_GetPCLK1Value() / ( 2 * pI2CHandle->I2C_Config.I2C_SCLSpeed ) );
+		tempreg |= (ccr_value & 0xFFF); // the 0xFFF is to mask out the rest of the 12:16 bits of uint_16 variable, because CCR is only 0:11 of the reg
+	}else
+	{
+		//mode is fast mode
+		tempreg |= ( 1 << 15);
+		tempreg |= (pI2CHandle->I2C_Config.I2C_FMDutyCycle << 14);
+		if(pI2CHandle->I2C_Config.I2C_FMDutyCycle == I2C_FM_DUTY_2)
+		{
+			ccr_value = (RCC_GetPCLK1Value() / ( 3 * pI2CHandle->I2C_Config.I2C_SCLSpeed ) );
+		}else
+		{
+			ccr_value = (RCC_GetPCLK1Value() / ( 25 * pI2CHandle->I2C_Config.I2C_SCLSpeed ) );
+		}
+		tempreg |= (ccr_value & 0xFFF); // the 0xFFF is to mask out the rest of the 12:16 bits of uint_16 variable, because CCR is only 0:11 of the reg
+	}
+	pI2CHandle->pI2Cx->CCR = tempreg;	
+
+}
+
+
+
+void I2C_MasterSendData(I2C_Handle_t *pI2CHandle,uint8_t *pTxbuffer, uint32_t Len, uint8_t SlaveAddr,uint8_t Sr)
+{
+	// 1. Generate the START condition
+	I2C_GenerateStartCondition(pI2CHandle->pI2Cx);
+
+	//2. confirm that start generation is completed by checking the SB flag in the SR1
+	//   Note: Until SB is cleared SCL will be stretched (pulled to LOW)
+	while( !  I2C_GetFlagStatus(pI2CHandle->pI2Cx,I2C_FLAG_SB)   );
+
+	//3. Send the address of the slave with r/nw bit set to w(0) (total 8 bits )
+	I2C_ExecuteAddressPhaseWrite(pI2CHandle->pI2Cx,SlaveAddr);
+
+	//4. Confirm that address phase is completed by checking the ADDR flag in teh SR1
+	while( !  I2C_GetFlagStatus(pI2CHandle->pI2Cx,I2C_FLAG_ADDR)   );
+
+	//5. clear the ADDR flag according to its software sequence
+	//   Note: Until ADDR is cleared SCL will be stretched (pulled to LOW)
+	I2C_ClearADDRFlag(pI2CHandle);
+
+	//6. send the data until len becomes 0
+
+	while(Len > 0)
+	{
+		while(! I2C_GetFlagStatus(pI2CHandle->pI2Cx,I2C_FLAG_TXE) ); //Wait till TXE is set
+		pI2CHandle->pI2Cx->DR = *pTxbuffer;
+		pTxbuffer++;
+		Len--;
+	}
+
+	//7. when Len becomes zero wait for TXE=1 and BTF=1 before generating the STOP condition
+	//   Note: TXE=1 , BTF=1 , means that both SR and DR are empty and next transmission should begin
+	//   when BTF=1 SCL will be stretched (pulled to LOW)
+
+	while(! I2C_GetFlagStatus(pI2CHandle->pI2Cx,I2C_FLAG_TXE) );
+
+	while(! I2C_GetFlagStatus(pI2CHandle->pI2Cx,I2C_FLAG_BTF) );
+
+
+	//8. Generate STOP condition and master need not to wait for the completion of stop condition.
+	//   Note: generating STOP, automatically clears the BTF
+	if(Sr == I2C_DISABLE_SR )
+		I2C_GenerateStopCondition(pI2CHandle->pI2Cx);
+
+}
